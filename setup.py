707597274e9deae6b24e84523c34d7fb6db290a6
David Hörl <hoerldavid@gmail.com>
2018-04-24 11:45:23 +0200

    release version 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='CalmUtils',
-    version='0.0.3-dev0',
+    version='0.0.3',
     description='commonly used Python code at the Center for Advanced Light Micsroscopy',
     long_description=open('README.rst').read(),
     author='David Hoerl',
