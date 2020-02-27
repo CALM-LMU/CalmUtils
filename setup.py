a8994c95afc0903b0358800fc0c81d4556b3639a
David Hörl <hoerldavid@gmail.com>
2020-02-27 17:15:02 +0100

    added more flexible Bioformats read functionality

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='CalmUtils',
-    version='0.0.12-dev0',
+    version='0.0.12-dev1',
     description='commonly used Python code at the Center for Advanced Light Micsroscopy',
     long_description=open('README.rst').read(),
     author='David Hoerl',
