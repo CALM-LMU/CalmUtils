9a56fb5c34efc027bc340b12774948ba3d4d4016
David Hoerl <hoerldavid@gmail.com>
2023-11-13 13:00:55 +0100

    corrected z direction reading, use nd2 for that

--- src/calmutils/imageio/nd2_helpers/nd2_metadata_helpers.py
+++ src/calmutils/imageio/nd2_helpers/nd2_metadata_helpers.py
@@ -1,7 +1,6 @@
-from operator import sub
-# TODO: unify to one library (preferentially nd2)
 from nd2 import ND2File
-from nd2reader import ND2Reader
+from nd2.structures import ZStackLoop
+
 
 def get_pixel_size(file_path):
     with ND2File(file_path) as reader:
@@ -17,16 +16,28 @@ def nd2_get_channel_names_cleaned(file_path, whitespace_replace_char='-'):
     with ND2File(file_path) as reader:
         return [s.channel.name.strip().replace(' ', whitespace_replace_char) for s in reader.metadata.channels]
 
+## OLD VERSION with nd2reader
+# from operator import sub
+# from nd2reader import ND2Reader
 
-def get_z_direction(file_path):
-    with ND2Reader(file_path) as reader:
-        if not 'z_coordinates' in reader.metadata or len(reader.metadata['z_coordinates']) < 2:
+# def get_z_direction(file_path):
+#     with ND2Reader(file_path) as reader:
+#         if not 'z_coordinates' in reader.metadata or len(reader.metadata['z_coordinates']) < 2:
+#             return None
+#         # difference of z position of first two planes -> z-spacing
+#         psz_z = sub(*reader.metadata['z_coordinates'][:2])
+#         # NOTE: earlier versions wrongly had to_sample if psz_z > 0, i.e. decreasing positions
+#         z_direction = 'to_sample' if psz_z < 0 else 'from_sample'
+#     return z_direction
+
+def get_z_direction(nd2_file):
+    with ND2File(nd2_file) as fd:
+        # try to find z loop metadata, return None if no info can be found
+        z_loop_meta = next((loop for loop in fd.experiment if isinstance(loop, ZStackLoop)), None)
+        if z_loop_meta is None:
             return None
-        # difference of z position of first two planes -> z-spacing
-        psz_z = sub(*reader.metadata['z_coordinates'][:2])
-        z_direction = 'to_sample' if psz_z > 0 else 'from_sample'
-    return z_direction
-
+        # string representation of direction
+        return 'bottom_to_top' if z_loop_meta.parameters.bottomToTop else 'top_to_bottom'
 
 if __name__ == '__main__':
     f = '/Users/david/Desktop/23AM03-02_4001.nd2'
