4f3fb266e68af76bcd6290b22a007a57f2506265
David Hoerl <hoerldavid@gmail.com>
2024-07-22 17:11:07 +0200

    release version 0.0.17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='CalmUtils',
-    version='0.0.17-dev',
+    version='0.0.17',
     description='commonly used Python code at the Center for Advanced Light Microscopy',
     long_description=open('README.rst').read(),
     author='David Hoerl',
